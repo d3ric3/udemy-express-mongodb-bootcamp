9e9ded0ed65f9cd0f7ee0d2dd60e9a59cb0bb62e
Katie Teow <katieteow@Katies-MacBook-Air.local>
2020-04-27 22:01:47 +0800

    Done Section 8 Number 93

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const Tour = require('./../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('DB connection success!');
+  });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// IMPORT DATA INTO DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// DELETE ALL DATA FROM DB
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+if (process.argv[2] === '--delete') {
+  deleteData();
+}
+
+if (process.argv[2] === '--import') {
+  importData();
+}
+
+// process.argv default value is
+//   [
+//     '/usr/local/bin/node',
+//     '/Users/katieteow/Desktop/udemy-express-mongodb-bootcamp/dev-data/data/import-dev-data.js'
+//   ]
