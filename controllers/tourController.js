0383fd0b856d5d14eba3d2e0ea8da377e7a29b4c
Katie Teow <katieteow@Katies-MacBook-Air.local>
2020-04-28 22:08:21 +0800

    Done Section 8 Number 99

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,12 @@
 const Tour = require('./../models/tourModel');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = 'price,-ratingsAverage';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     // BUILD QUERY
@@ -33,6 +40,20 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      await (async function() {
+        if (skip >= numTours) throw new Error('This page does not exists!');
+      })();
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
@@ -46,7 +67,7 @@ exports.getAllTours = async (req, res) => {
   } catch (err) {
     res.status(404).json({
       status: 'fail',
-      message: err
+      message: err instanceof Error ? err.message : err
     });
   }
 };
@@ -63,6 +84,7 @@ exports.getTour = async (req, res) => {
       }
     });
   } catch (err) {
+    res.send(err);
     res.status(404).json({
       status: 'fail',
       message: err
