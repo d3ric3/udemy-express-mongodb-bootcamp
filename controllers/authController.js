44ccc12561fbcda1f078c99da050f2462f3c2a89
Derice <d3ric3@gmail.com>
2020-05-26 00:36:45 +0800

    Done Section 10 Number 145

--- controllers/authController.js
+++ controllers/authController.js
@@ -24,7 +24,21 @@ const signToken = id => {
 
 const createSendToken = catchAsync(async (user, statusCode, res) => {
   const token = await signToken(user._id);
+  // expires is in milliseconds
+  // secure: whether to send in SSL/HTTPS
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    secure: false,
+    httpOnly: true
+  };
 
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  // remove the password from the output
   user.password = undefined;
 
   res.status(statusCode).json({
