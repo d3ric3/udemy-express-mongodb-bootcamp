82b78e1e95564c3f5ad6d7175f5d72e2fe1efdf2
Derice <d3ric3@gmail.com>
2020-06-21 22:59:51 +0800

    Done Section 11 Number 160

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,6 +2,7 @@ const Tour = require('./../models/tourModel');
 const AppError = require('./../utils/appError');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
+const factory = require('./handlerFactory');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -33,7 +34,7 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 });
 
 exports.getTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findById(req.params.id);
+  const tour = await Tour.findById(req.params.id).populate('reviews');
   // Tour.findOne({ _id: req.params.id });
 
   if (!tour) {
@@ -77,15 +78,7 @@ exports.updateTour = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.deleteTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findByIdAndDelete(req.params.id);
-
-  if (!tour) {
-    return next(new AppError('No tour found with that ID', 404));
-  }
-
-  res.status(204).send();
-});
+exports.deleteTour = factory.deleteOne(Tour);
 
 exports.getTourStats = catchAsync(async (req, res, next) => {
   const stats = await Tour.aggregate([
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,8 +2,11 @@ const express = require('express');
 const authController = require('./../controllers/authController');
 const reviewController = require('./../controllers/reviewController');
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
+// POST /tour/2122j424as/reviews
+// GET /tour/2122j424as/reviews
+// POST /reviews
 router
   .route('/')
   .get(reviewController.getAllReviews)
@@ -12,5 +15,7 @@ router
     authController.restrictTo('user'),
     reviewController.createReview
   );
+
+router.route('/:id').delete(reviewController.deleteReview);
 
 module.exports = router;
